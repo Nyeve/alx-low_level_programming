97e4f90bce4052ba4124d8a33fdf644b2b265d04
Nyeve <eveanyaboh@gmail.com>
2022-03-17 14:23:47 -0700

    multiply

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
  * main - check for upper case letter
- * @c : character to check the case 
- * 
+ * @c : character to check the case  
  * Return: 0 or 1
  */
 int _isupper(int c)
