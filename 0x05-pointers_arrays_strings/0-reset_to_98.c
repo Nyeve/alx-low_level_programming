c0efca98efbe10457fbae2b41724d0146db1ceda
Nyeve <eveanyaboh@gmail.com>
2022-03-21 12:10:30 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,10 @@
 #include<stdio.h>
 /**
- * reset_to_98- takes a pointer to an int as parameter and updates the value it points to 98
+ * reset_to_98: takes a pointer to an int as parameter and updates the value
+ * @n: pointer
+ * Return:void
  */
-
-int reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	int n= 98;
-	int *p= &n;
-
-	return (0);
-
-
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,12 @@
-#ifndef "MAIN>H"                                                                                      
-#define "MAIN.H"                                                                                      
-                                                                                                
-void reset_to_98(int *n);                                                                             
-
-                                                                                                      
-
-#endif  
+#ifndef "MAIN.H"
+#define "MAIN.H"
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing declarations for all functions
+ *
+ * used in the 0x05-pointer_arrays_strings directory.
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
