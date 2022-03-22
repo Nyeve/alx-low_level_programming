0bd861422e9bba5bb0bb0cc825fc8a59c5a0be25
Nyeve <eveanyaboh@gmail.com>
2022-03-22 03:11:47 +0300

    all dir

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+/*
+ * File: 3-print_alphabets.c
+ */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (y = 'A'; y <= 'Z'; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+/*
+ *
+ *   File: 5-print_numbers.c
+ *
+ *   Auth: Everlyne Angwenyi
+ *
+ */
+#include <stdio.h>
+/**
+ * main- prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	do {
+		printf("%d", x);
+		x++;
+	} while (x < 10);
+	printf("\n");
+	return (0);
+}
