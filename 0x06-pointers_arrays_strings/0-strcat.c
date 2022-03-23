896e770da639917c84c05e337870d1ae9c56bfa5
Nyeve <eveanyaboh@gmail.com>
2022-03-23 17:01:37 +0300

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 /**
+ * *_strcat- appends the src string to the dest string
  *
- *
- *
- *
+ * @char- pointer
+ * Return: a pointer
  */
 char *_strcat(char *dest, char *src)
 {
-	char *dest;
-	char *src;
-
+	char *dest = &s1;
+	char *src = &s2;
+    {
+    printf("Value of dest:"%d\'0'", *dest)
+    printf("Value of src:"%d\n", *src)
+      
+      strcat (*dest, *src);
+            printf("strcat (*dest, *src): %s\'0'");
+    }
+    return (0);
 }
