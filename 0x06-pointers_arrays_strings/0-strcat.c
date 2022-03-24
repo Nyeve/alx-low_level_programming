91c207c5ede8a8a26da61c832839fb2e8f043aaf
Nyeve <eveanyaboh@gmail.com>
2022-03-24 09:07:03 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,12 @@
 char *_strcat(char *dest, char *src)
 {
 	char *new_dest = dest;
-		/* navigate to the end of dest */
 
-		while (*dest)
-			dest++;/* copy all characters of src to dest */
-			while (*src)
-				*dest++ = *src++;
+	while (*dest)
+		dest++;/* copy all characters of src to dest */
+	while (*src)
+		*dest++ = *src++;
 				/* add terminating null byte */
-				*dest = '\0';
-					return (new_dest);
+	*dest = '\0';
+	return (new_dest);
 }
