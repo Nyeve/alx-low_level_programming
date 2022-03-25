e89aedc481885f7055a21d36d6e2b2b9f2ffb121
Nyeve <eveanyaboh@gmail.com>
2022-03-25 14:44:30 -0700

    5

--- 0x06-pointers_arrays_strings/5-main.c
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -8,12 +8,11 @@
  */
 int main(void)
 {
-    char str[] = "Look up!\n";
-    char *ptr;
+	char str[] = "Look up!\n";
+	char *ptr;
 
-    ptr = string_toupper(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+	ptr = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
 }
-julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-string_toupper.c -o 5-string_toupper
