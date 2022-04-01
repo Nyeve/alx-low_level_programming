aa64d0935b12a1745bb4e2ff7d7c4160b8253355
Nyeve <eveanyaboh@gmail.com>
2022-03-31 23:45:32 -0700

    7-8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -57,4 +57,5 @@ int is_palindrome(char *s)
 
 	if (!(*s))
 		return (1);
+	return (check_palindrome(s, len, index));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+void _puts_recursion(char *s);
+
+/**
+ * File: main.h
+ *
+ * Auth: EVerlyne Angwenyi
+ *
+ * Desc: Header file containing prototypes for all functions
+ *
+ *       used in the 0x08-recursion directory.
+ */
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -9,10 +22,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-<<<<<<< HEAD
-_putchar(char c);
+
 #endif
-=======
-int _putchar(char c);
-#endif
->>>>>>> 69011e56230c781f3b9fa6d72fcad5e3bdfea666
