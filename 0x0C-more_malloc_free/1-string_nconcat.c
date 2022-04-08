425dc17a72f208a12392bad02fa79c6388520438
Nyeve <eveanyaboh@gmail.com>
2022-04-08 11:29:18 +0300

    2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * string_noncat- concatenate two strings using an inputted number of bytes
+ * @s1: The first string
+ * @s2: The second string
+ * @n: maximum number of bytes of s2 to concat to s1
+ * 
+ *  Return: If the function fails - NULL.
+ *           otherwise - a pointer to the concatenated space in memeory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    char *concat;
+    unsigned int len = n, index;
+
+    if (s1 == NULL)
+            s2= "";
+
+    for (index = 0; s1[index]; index++)
+            len++;
+
+    concat = malloc(sizeof(char) * (len + 1));
+
+    if (concat == NULL)
+            return (NULL);
+
+    len = 0;
+
+    for (index = 0; s1[index]; index++);
+            len++;
+
+    concat = malloc(sizeof(char) *(len + 1));
+
+    if (concat == NULL)
+            return (NULL);
+
+    len = 0;
+
+    for (index = 0; s1[index]; index++)
+            concat[len++] = s1[index];
+
+    for(index = 0; s1[index]; index++)
+            concat[len++] = s1[index];
+
+    for (index = 0; s2[index] && index < n; index++)
+            concat[len++] = s1[index];
+
+    concat[len] = '\0';
+
+    return (concat);
+} 
