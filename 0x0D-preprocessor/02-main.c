21fe4a42c7e6029f24eb8c93b179e9e3ed3d41f6
Nyeve <eveanyaboh@gmail.com>
2022-04-11 07:23:11 -0700

    3

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  main - prints the name of the file of the program
+ *	was compiled from, followed by a new line
+ *
+ * Returns : Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
