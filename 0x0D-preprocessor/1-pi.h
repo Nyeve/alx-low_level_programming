7f44a5f745adbd49e8f714f3e263334b39403621
Nyeve <eveanyaboh@gmail.com>
2022-04-11 06:31:14 -0700

    2

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_HEADER_FILE
+#define PI_HEADER_FILE
+/*
+ * File: 1-pi.h
+ *
+ * Desc: Header file that defines a macro named PI
+ *
+ *       as an abbreviation for the token 3.14159265359.
+ */
+#define PI 3.14159265359
+
+#endif
